8cc83ef8f16b5bf558110b24885b91e2eee13a87
i.shirinov <shirinovilyos6965@gmail.com>
2022-11-21 16:28:13 +0500

    № 3

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -3,5 +3,12 @@ package lang.print.gaps.task3;
 public class DeclaringVars {
     public static void main(String[] args) {
         //write code here
+
+        int a = 1;
+        int b = 10;
+        int c = 100;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -3,5 +3,11 @@ package lang.print.gaps.task3;
 public class ReassigningValues {
     public static void main(String[] args) {
         //write code here
+        int a = 1;
+        int b = 10;
+        int c = 100;
+        System.out.println("linkToFirst: " + a);
+        System.out.println("linkToSecond: " + b);
+        System.out.println("linkToThird : " + c);
     }
 }
